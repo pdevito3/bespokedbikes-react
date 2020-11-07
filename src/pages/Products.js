938a60cb1ee65b2cfcc924247ba254034afb41e0
Paul DeVito <pdevito3@gmail.com>
2020-11-07 13:31:12 -0500

    ui cleanup

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -28,7 +28,7 @@ function Products(props){
         </div>
         
         { (productsQuery.isSuccess || productsQuery.isLoading) && (
-          <div className="flex flex-col">
+          <div className="pt-5 flex flex-col">
             <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-t-lg">
